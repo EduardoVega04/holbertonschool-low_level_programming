5a64d0b570d72f38d23e97b3a082aab7073006c0
EduardoVega04 <eduarvega04@hotmail.com>
2020-02-06 21:48:26 +0000

    Printing the alphabet with putchar

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,23 @@
 #include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main()
 {
 char a = 'a';
+char b = 'A';
 while (a <= 'z')
 {
 putchar(a);
 a++;
 }
-return 0;
+while (b <= 'Z')
+{
+putchar(b);
+b++;
+}
+putchar('\n');
+return (0);
 }
