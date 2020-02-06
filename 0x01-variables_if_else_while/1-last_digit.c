adffccf0f16d91ed074da5c07f215eaad36dc4a4
EduardoVega04 <eduarvega04@hotmail.com>
2020-02-06 20:53:42 +0000

    if, else, else if

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,18 +14,18 @@ n = rand() - RAND_MAX / 2;
 
 printf("The last digit of ");
 printf("%d ", n);
-printf("is");
+printf("is %d ", n % 10);
 
 if ((n % 10) > 5)
 {
-printf(" %i and is greater than 5\n", (n % 10));
+printf("and is greater than 5\n");
 }
 else if ((n % 10) == 0)
 {
-printf(" %i and is 0\n", (n % 10));
+printf("and is 0\n");
 }
 else
-printf(" %i and is less than 6 and not 0\n", (n % 10));
+printf("and is less than 6 and not 0\n");
 
 return (0);
 }
