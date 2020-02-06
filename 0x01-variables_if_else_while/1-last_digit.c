4ee206278082c9e0344e2d9001f2fa840666056b
EduardoVega04 <eduarvega04@hotmail.com>
2020-02-06 20:47:14 +0000

    if, else, else if

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,14 +18,14 @@ printf("is");
 
 if ((n % 10) > 5)
 {
-printf(" %d and is greater than 5\n", (n % 10));
+printf(" %i and is greater than 5\n", (n % 10));
 }
 else if ((n % 10) == 0)
 {
-printf(" %d and is 0\n", (n % 10));
+printf(" %i and is 0\n", (n % 10));
 }
 else
-printf(" %d and is less than 6 and not 0\n", (n % 10));
+printf(" %i and is less than 6 and not 0\n", (n % 10));
 
 return (0);
 }
