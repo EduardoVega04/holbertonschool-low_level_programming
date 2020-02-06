21c3c3fc5b46103537370ac9001205d422dd33e0
EduardoVega04 <eduarvega04@hotmail.com>
2020-02-06 23:15:29 +0000

    Printing numbers with commas

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,10 +9,18 @@ int main(void)
 char n = 0;
 while (n <= 9)
 {
+if (n < 9)
+{
 putchar(n + '0');
 putchar(',');
 putchar(' ');
 n++;
+}
+else
+{
+putchar(n + '0');
+n++;
+}
 }
 putchar('\n');
 return (0);
