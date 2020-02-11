fd6202470819e420414a936fe117e47ff697cfd4
EduardoVega04 <eduarvega04@hotmail.com>
2020-02-11 18:47:57 +0000

    Is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _isalpha(int c) - entry point
+ *
+ * Checks if is lowercase or not
+ * Return: 1 if c is lowercase, otherwise return 0
+ *
+ */
+
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z';)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
