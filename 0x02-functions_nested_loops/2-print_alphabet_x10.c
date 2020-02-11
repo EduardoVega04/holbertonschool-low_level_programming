58b9d76364c7c8e7a3d096458c651c7d33efe0dd
EduardoVega04 <eduarvega04@hotmail.com>
2020-02-11 17:22:18 +0000

    Print the alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,16 +8,16 @@
  */
 void print_alphabet_x10(void)
 {
+{
 char alphabet;
 int veces;
-while (veces < 10)
+for (veces = 0; veces < 10; veces++)
 {
-veces++;
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
-_putchar (alphabet);
+_putchar(alphabet);
 }
-_putchar ('\n');
+_putchar('\n');
 }
-return (0);
+}
 }
