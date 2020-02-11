625ffa2d5e096889aa25cdfb4fb4ff129d32450b
EduardoVega04 <eduarvega04@hotmail.com>
2020-02-11 16:21:38 +0000

    Putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char h[] = "Holberton";
+int i;
+for (i = 0; i < 9; i++)
+{
+_putchar(h[i]);
+}
+_putchar('\n');
+return (0);
+}
