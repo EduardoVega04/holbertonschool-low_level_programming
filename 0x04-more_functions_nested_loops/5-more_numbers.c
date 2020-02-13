49bd6bc43ef0504291473157bf35f2057aea74c8
EduardoVega04 <eduarvega04@hotmail.com>
2020-02-13 17:08:20 +0000

    Is upper

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _more_numbers - Print from 0 to 14 ten times
+ *
+ * Return: Nothing
+ *
+ */
+void more_numbers(void)
+{
+  int i;
+  int j;
+  for (i = 0; i <=  
+}
