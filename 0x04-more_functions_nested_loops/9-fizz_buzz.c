3d53e8e50220d1a702b5e2081c37bbb350bea24b
EduardoVega04 <eduarvega04@hotmail.com>
2020-02-13 23:35:19 +0000

    Print from 1 to 100 with exceptions

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if ((i % 3) == 0)
+{
+printf("Fizz");
+printf(" ");
+}
+else if ((i % 5) == 0)
+{
+printf("Buzz");
+printf(" ");
+}
+else if (((i % 5) == 0) && ((i % 3) == 0))
+{
+printf("FizzBuzz");
+printf(" ");
+}
+else
+{
+printf("%d", i);
+printf(" ");
+}
+}
+printf("\n");
+return (0);
+}
