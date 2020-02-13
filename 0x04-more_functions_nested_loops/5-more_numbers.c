e2b8276f8945cf0d1adcab23b1b000db06d21753
EduardoVega04 <eduarvega04@hotmail.com>
2020-02-13 19:43:40 +0000

    Print from 0 to 14 with putchar

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,7 +7,17 @@
  */
 void more_numbers(void)
 {
-  int i;
-  int j;
-  for (i = 0; i <=  
+int x, y;
+for (x = 0; x <= 10; x++)
+{
+for (y = 0; y <= 14; y++)
+{
+if (y > 9)
+{
+_putchar((y / 10) + '0');
+}
+_putchar((y % 10) + '0');
+}
+_putchar('\n');
+}
 }
