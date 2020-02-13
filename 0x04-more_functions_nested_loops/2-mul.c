9fadbdd78a85886176d6b78aab85a1fd92b0a547
EduardoVega04 <eduarvega04@hotmail.com>
2020-02-13 15:46:37 +0000

    Multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * mul - Multiplies two characters
+ * @a: The character to multiply
+ * @b: The character to multply
+ * Return: The multiplication of both numbers
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
