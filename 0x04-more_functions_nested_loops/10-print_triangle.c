67fd3a5bd54d68c9abfff433e26ab311d6ffa642
EduardoVega04 <eduarvega04@hotmail.com>
2020-02-14 04:30:17 +0000

    Print a triangle of n size

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#nclude "holberton.h"
+/**
+ * print_triangle - Prints a triangle of n size
+ * @size: The integer to test
+ *
+ * Return: Nothing
+ *
+ */
+void print_triangle(int size)
+{
+int i;
+int j;
+
+if (size <= 0)
+{
+putchar ('\n');
+}
+for (i = 1; i <= size; i++)
+{
+for (j = 1; j <= size; j++)
+{
+if (j <= (size - i))
+{
+putchar (' ');
+}
+else
+{
+putchar ('#');
+}
+}
+putchar ('\n');
+}
+}
