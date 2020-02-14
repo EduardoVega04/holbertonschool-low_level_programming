17ac61d9a0f3e814f6079f0994da0fe7118a22c2
EduardoVega04 <eduarvega04@hotmail.com>
2020-02-14 00:00:24 +0000

    Print from 0 to 100 with exceptions

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,23 +14,23 @@ for (i = 1; i <= 100; i++)
 {
 if (((i % 5) == 0) && ((i % 3) == 0))
 {
-printf(" ");
 printf("FizzBuzz");
 }
 else if ((i % 3) == 0)
 {
-printf(" ");
 printf("Fizz");
 }
 else if ((i % 5) == 0)
 {
-printf(" ");
 printf("Buzz");
 }
 else
 {
-printf(" ");
 printf("%d", i);
+}
+if(i < 100)
+{
+printf(" ");
 }
 }
 printf("\n");
