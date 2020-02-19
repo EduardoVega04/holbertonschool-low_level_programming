36751b21662ebee08f117a55f24eef7a1139f250
EduardoVega04 <eduarvega04@hotmail.com>
2020-02-19 04:57:28 +0000

    Print n elements of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_array - Prints n elements of an array
+ * @a: the integer to test
+ * @n: the integer to test
+ * Return: Nothing
+ */
+
+void print_array(int *a, int n)
+{
+int var = 0;
+for (n--; n >= 0; n--, var++)
+{
+printf("%d", a[var]);
+if (n > 0)
+{
+printf(", ");
+}
+}
+printf("\n");
+}
