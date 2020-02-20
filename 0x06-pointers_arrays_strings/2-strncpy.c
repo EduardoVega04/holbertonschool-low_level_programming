08d83493cdd0068cc27a537eb5d99e6c548b1ecc
EduardoVega04 <eduarvega04@hotmail.com>
2020-02-20 22:12:07 +0000

    Copy a string into another

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,19 @@
 #include "holberton.h"
+/**
+ * _strncpy - Copy a string into another
+ * @dest: The character to test
+ * @src: The character to test
+ * @n: The n number of bytes to manipulate
+ * Return: Pointer to dest
+ *
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int a = 0;
+while (a < n)
+{
+dest[a] = src[a];
+a++;
+}
+return (dest);
+}
