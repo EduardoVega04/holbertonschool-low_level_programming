8ca2217333931a944d579817194e2f4d35273f21
EduardoVega04 <eduarvega04@hotmail.com>
2020-02-26 20:14:55 +0000

    Print a string in reverse with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,10 +7,9 @@
  */
 void _puts_recursion(char *s)
 {
-int i = 0;
 if (*s)
 {
-_putchar(*(s + i));
+_putchar(*s);
 s++;
 _puts_recursion(s);
 }
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 /**
- * _puts_recursion - Prints a string in reverse
+ * _print_rev_recursion - Prints a string in reverse
  * @s: The string to test
  * Return: nothing
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 if (*s)
 {
-_puts_recursion(s + 1);
-putchar(*s);
+_print_rev_recursion(s + 1);
+_putchar(*s);
 }
 }
