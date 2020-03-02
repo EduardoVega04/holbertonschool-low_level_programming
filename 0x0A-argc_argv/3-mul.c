d31bcc71ddadf6b2e34603fe8735a7de8f2cb1fa
EduardoVega04 <eduarvega04@hotmail.com>
2020-03-02 01:13:23 +0000

    Multiply two numbers of compiling arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,11 +12,12 @@ int main(int argc, char *argv[])
 	int num1 = atoi(argv[1]);
 	int num2 = atoi(argv[2]);
 
-	if (argc != 2)
+	if (argc == 3)
 	{
-		printf("Error\n");
-		return (1);
+		printf("%d\n", num1 * num2);
+		return (0);
 	}
-	printf("%d\n", num1 * num2);
-	return (0);
+	else
+		printf("Error\n");
+	return (1);
 }
