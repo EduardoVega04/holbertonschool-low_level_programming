385ee3e67de5c66de99348e9d018f2427b03c73c
EduardoVega04 <eduarvega04@hotmail.com>
2020-03-02 01:05:26 +0000

    Multiplies two numbers as compilation arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point. Multiplies two numbers
+ * @argc: The number of arguments of program when executed
+ * @argv: The string with arguments of program when executed
+ * Return: 0
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+
+	if (argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", num1 * num2);
+	return (0);
+}
