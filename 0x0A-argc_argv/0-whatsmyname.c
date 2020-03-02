c8250b949f0ba318eb9a61c2fb7e0e00a016cdd1
EduardoVega04 <eduarvega04@hotmail.com>
2020-03-02 00:07:57 +0000

    Print name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: The number of arguments of program when executed
+ * @argv: The string with arguments of program when executed
+ * Return: 0
+ *
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+<strong>argc, argv<strong>
