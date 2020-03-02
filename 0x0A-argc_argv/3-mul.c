60d41b7ee49cd91d251d9d6e3cc19c86eaec2f79
EduardoVega04 <eduarvega04@hotmail.com>
2020-03-02 02:05:57 +0000

    Multiply the parameters of the compiling program

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,15 +9,17 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-
 	if (argc == 3)
 	{
+		int num1 = atoi(argv[1]);
+		int num2 = atoi(argv[2]);
+
 		printf("%d\n", num1 * num2);
 		return (0);
 	}
 	else
+	{
 		printf("Error\n");
-	return (1);
+		return (1);
+	}
 }
