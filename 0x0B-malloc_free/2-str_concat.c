d00c99f1fb9c2b4c9b16dc5f1dfdc0151e4b2504
EduardoVega04 <eduarvega04@hotmail.com>
2020-03-03 20:18:54 +0000

    Concatenate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,11 +14,11 @@ char *str_concat(char *s1, char *s2)
 	int b;
 	char *ptr;
 
-	if(s1 == NULL)
+	if (s1 == NULL)
 	{
 		s1 = "";
 	}
-	if(s2 == NULL)
+	if (s2 == NULL)
 	{
 		s2 = "";
 	}
@@ -43,5 +43,6 @@ char *str_concat(char *s1, char *s2)
 	{
 		ptr[a + b] = s2[b];
 	}
+
 	return (ptr);
 }
