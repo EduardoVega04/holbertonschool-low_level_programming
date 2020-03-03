42fdbaf8e7db716d795110d68cfda69c8b9d50d3
EduardoVega04 <eduarvega04@hotmail.com>
2020-03-03 18:47:35 +0000

    Copy a string into another

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * _strdup - Creates an array of chars.
+ * @str: Array of characters
+ *
+ * Return: NULL if str is NULL, Returns a pointer to srt
+ *         or NULL if it fails.
+ */
+char *_strdup(char *str)
+{
+	if (str == NULL)
+		return (NULL);
+
+	char *ptr;
+	int i;
+
+	ptr = (char *)malloc(sizeof(str));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		ptr[i] = str[i];
+	}
+	return (ptr);
+}
