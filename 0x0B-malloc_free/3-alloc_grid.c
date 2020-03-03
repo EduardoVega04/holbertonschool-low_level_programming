726db3bb5a40b8e147f94010c1dcbfe36400be75
EduardoVega04 <eduarvega04@hotmail.com>
2020-03-03 23:13:53 +0000

    Create and initialize a two dimention array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+/**
+ * alloc_grid - Returns a pointer to a 2d array.
+ * @width: Width of the array
+ * @height: Height of the array
+ *
+ * Return: Return pointer to the array.
+ */
+int **alloc_grid(int width, int height)
+{
+	int a;
+	int i;
+	int j;
+	int **ptr;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	ptr = (int **)malloc(sizeof(int) * height);
+
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	for (a = 0; a < height; a++)
+	{
+		ptr[a] = (int *)malloc(sizeof(int) * width);
+		if (ptr[a] == NULL)
+		{
+			free(ptr);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			ptr[i][j] = 0;
+		}
+	}
+	return (ptr);
+}
