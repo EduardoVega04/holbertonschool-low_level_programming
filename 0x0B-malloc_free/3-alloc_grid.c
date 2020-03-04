5a720c93983d1d9416c1ef2eb1ff05b30975b886
EduardoVega04 <eduarvega04@hotmail.com>
2020-03-04 00:43:23 +0000

    Create and initialize a two dimention array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,6 +26,8 @@ int **alloc_grid(int width, int height)
 		m[i] = (int *) malloc(width * sizeof(int));
 		if (m[i] == NULL)
 		{
+			for (; i >= 0; i--)
+				free(m[i]);
 			free(m);
 			return (NULL);
 		}
