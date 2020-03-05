4c51d2eed946732e0ead60dde5c60a633b7e140b
EduardoVega04 <eduarvega04@hotmail.com>
2020-03-05 23:15:58 +0000

    Reallocate memory

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -22,6 +22,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		ptr = malloc(new_size + old_size);
 		if (ptr == NULL)
 			return (NULL);
+		return (ptr);
 	}
 
 	if (new_size > old_size)
@@ -29,6 +30,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		ptr = malloc(new_size);
 		if (ptr == NULL)
 			return (NULL);
+		return (ptr);
 	}
 	return (ptr);
 }
