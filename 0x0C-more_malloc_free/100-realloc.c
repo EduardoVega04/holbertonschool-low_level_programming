dd20d9fbe6ef9a7f8aa586ec0687cf3e5fdb8c9b
EduardoVega04 <eduarvega04@hotmail.com>
2020-03-05 22:48:19 +0000

    Reallocate space in memory

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * _realloc - Reallocates a space in memory
+ * @ptr: The pointer with allocated size in memory
+ * @old_size: Current allocated size
+ * @new_size: The new size to realloc
+ * Return: Nothing
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size + old_size);
+		if (ptr == NULL)
+			return (NULL);
+	}
+
+	if (new_size > old_size)
+	{
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+			return (NULL);
+	}
+}
--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -11,3 +11,4 @@ int get_string (char *s);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void simple_print_buffer(int *buffer, unsigned int size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
