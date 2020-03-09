d957d6de5c1123347caf6a81dc6fa9b5897d3f01
EduardoVega04 <eduarvega04@hotmail.com>
2020-03-09 21:40:43 +0000

    Create a structure

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+<strong>Structures and typedef<strong>
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H_
+#define DOG_H_
+/**
+ * struct dog - Structure called dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
