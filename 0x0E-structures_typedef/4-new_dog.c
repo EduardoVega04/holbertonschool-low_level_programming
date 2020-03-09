1e94462a8ba17c1c542dd7939ad92e9aa01b42c4
EduardoVega04 <eduarvega04@hotmail.com>
2020-03-09 23:45:50 +0000

    Creates a new structure

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+/**
+ * new_dog - Creates a new structure dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ * Return: Return a pointer to the structure
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ptr;
+
+	ptr = malloc(sizeof(dog_t));
+	if (ptr == NULL)
+		return (NULL);
+
+	(*ptr).name = name;
+	(*ptr).age = age;
+	(*ptr).owner = owner;
+
+	return (ptr);
+}
