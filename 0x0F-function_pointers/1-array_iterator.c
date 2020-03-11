262bc33ab8f9b7f089c8c700a4e3d502d8882eb9
EduardoVega04 <eduarvega04@hotmail.com>
2020-03-11 19:54:26 +0000

    Print an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - Function that prints arrays
+ * @array: Array to print
+ * @size: Size of array
+ * @action: Pointer to function
+ * Return: Nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
