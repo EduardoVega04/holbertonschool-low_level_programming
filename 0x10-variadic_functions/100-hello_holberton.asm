8cd8e61ad59a641ef8395a7fcf945462f1134c12
EduardoVega04 <eduarvega04@hotmail.com>
2020-03-14 19:09:18 +0000

    Hello Holberton in assembly

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,18 +1,26 @@
-global main
+section .text                   ;section declaration
 
-	section .text
+	;; we must export the entry point to the ELF linker or
+	    global main	;loader. They conventionally recognize _start as their
+	;; entry point. Use ld -e foo to override the default.
 
 main:
-	  mov rax, 1        	; write(
-	  mov rdi, 1        	;   STDOUT_FILENO,
-	  mov rsi, msg      	;   "Hello, Holberton\n",
-	  mov rdx, msglen   	;   sizeof("Hello, Holberton\n")
-	  syscall           	; );
 
-	  mov rax, 60       	; exit(
-	  mov rdi, 0        	;   EXIT_SUCCESS
-	  syscall           	; );
+	;; write our string to stdout
 
-	section .rodata
-msg:	 db "Hello, Holberton!", 10
-msglen:	 equ $ - msg
+	    mov     edx,len	;third argument: message length
+	    mov     ecx,msg	;second argument: pointer to message to write
+	    mov     ebx,1	;first argument: file handle (stdout)
+	    mov     eax,4	;system call number (sys_write)
+	    int     0x80	;call kernel
+
+	;; and exit
+
+	mov     ebx,0		;first syscall argument: exit code
+	    mov     eax,1	;system call number (sys_exit)
+	    int     0x80	;call kernel
+
+	section .data		;section declaration
+
+	msg db      "Hello, Holberton",0xa ;our dear string
+	len equ     $ - msg             ;length of our dear string
