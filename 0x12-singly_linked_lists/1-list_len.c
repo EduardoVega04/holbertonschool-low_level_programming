ce1df6adcde94843012bfcf868afc39b9f2b5ead
EduardoVega04 <eduarvega04@hotmail.com>
2020-03-20 02:03:07 +0000

    Print the number of elements of a list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * list_len - prints the number of elements of a list
+ * @h: head of the linked list
+ * Return: the number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t contador = 2;
+
+	return (contador);
+}
