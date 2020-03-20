7f75f3abd065c271f4e26105b226927d9ead8570
EduardoVega04 <eduarvega04@hotmail.com>
2020-03-20 04:04:59 +0000

    Print something before main executes

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * function - prints text before main function executes
+ */
+
+void __attribute__ ((constructor)) function(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
