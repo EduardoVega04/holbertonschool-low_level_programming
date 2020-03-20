5ab357ebb53662628a2a169969373f04c4f01935
EduardoVega04 <eduarvega04@hotmail.com>
2020-03-20 01:35:14 +0000

    Print the elements of a linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a linked list
+ * @h: head of the linked list
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t nodos = 0;
+
+	while (h != NULL)
+	{
+		if ((*h).str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+
+		printf("[%d] %s\n", (*h).len, (*h).str);
+
+		nodos++;
+		h = (*h).next;
+	}
+	return (nodos);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+<strong>Linked lists<strong>
