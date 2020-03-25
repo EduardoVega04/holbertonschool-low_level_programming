97459ab879da9c9b7a37327659292b7d8223e40d
EduardoVega04 <eduarvega04@hotmail.com>
2020-03-25 02:03:36 +0000

    Frees a linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a list
+ * @head: head of list
+ * Return: Nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *ptr;
+
+	while (head != NULL)
+
+	{
+		ptr = head->next;
+		free(head);
+		head = ptr;
+	}
+}
