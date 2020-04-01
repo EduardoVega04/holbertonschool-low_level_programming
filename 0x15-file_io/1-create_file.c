e747732091d78e4b132133795b4f7032065b1d15
EduardoVega04 <eduarvega04@hotmail.com>
2020-04-01 02:15:51 +0000

    Create a file and fill it with text

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * create_file - Reads a text file and prints to POSIX stdout
+ * @filename: Name of the file
+ * @text_content: Text to write to the file
+ *
+ * Return: Numbers of bytes printed
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int crear;
+	int llenar;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	crear = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (crear < 0)
+	{
+		return (-1);
+	}
+
+	llenar = write(crear, text_content, strlen(text_content));
+
+	if (llenar < 0)
+	{
+		return (-1);
+	}
+	return (llenar);
+}
