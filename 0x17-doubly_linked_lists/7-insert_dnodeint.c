dddc13a4a50f560c0e22ff06716c4971148ec892
EduardoVega04 <eduarvega04@hotmail.com>
2020-05-02 07:54:38 +0000

    Doubly linked lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,7 +23,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (new_node);
 	}
 
-	if (idx == 0)
+	if (idx <= 0)
 	{
 		new_node->next = *h;
 		(*h)->prev = new_node;
