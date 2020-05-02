a77d68a3c9ec0d06060d5bdc3058a09bdc32164d
EduardoVega04 <eduarvega04@hotmail.com>
2020-05-02 08:36:27 +0000

    Add node at given index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,6 +16,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
 
 	if (*h == NULL)
 	{
@@ -23,16 +25,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (new_node);
 	}
 
-	if (idx <= 0)
+	if (idx == 0)
 	{
 		new_node->next = *h;
 		(*h)->prev = new_node;
 		*h = new_node;
 		return (new_node);
 	}
-
-	tmp = tmp->next;
-	pos++;
 
 	while (tmp != NULL)
 	{
@@ -46,13 +45,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		}
 
 		pos++;
-		prev_tmp = prev_tmp->next;
+		prev_tmp = tmp;
 		tmp = tmp->next;
 	}
-	pos--;
 	prev_tmp->next = new_node;
 	new_node->prev = prev_tmp;
-	new_node->next = NULL;
 	return (new_node);
 }
 
