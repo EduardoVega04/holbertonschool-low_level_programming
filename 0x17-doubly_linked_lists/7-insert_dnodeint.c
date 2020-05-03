e31626f245f8f66dd06c5a7d300e3ddcb19fd7f6
EduardoVega04 <eduarvega04@hotmail.com>
2020-05-03 04:34:36 +0000

    Add a node at a given index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,49 +8,43 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node = NULL, *tmp = *h, *prev_tmp = *h;
+	dlistint_t *new_node = NULL, *tmp = NULL, *prev_tmp = NULL;
 	unsigned int pos = 0;
+
+	if (h == NULL)
+		return (NULL);
+
+	tmp = *h;
+	prev_tmp = *h;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = NULL;
-
-	if (*h == NULL)
-	{
-		/**h = new_node;
-		  return (new_node);*/
-		return (NULL);
-	}
 
 	if (idx == 0)
 	{
-		new_node->next = *h;
-		(*h)->prev = new_node;
-		*h = new_node;
+		new_node = add_dnodeint(h, n);
 		return (new_node);
 	}
 
-	while (tmp != NULL)
+	while (tmp != NULL && pos != idx)
 	{
-		if (pos == idx)
-		{
-			prev_tmp->next = new_node;
-			new_node->prev = prev_tmp;
-			new_node->next = tmp;
-			tmp->prev = new_node;
-			return (new_node);
-		}
-
 		pos++;
 		prev_tmp = tmp;
 		tmp = tmp->next;
 	}
-	prev_tmp->next = new_node;
-	new_node->prev = prev_tmp;
-	return (new_node);
+
+	if (pos == idx)
+	{
+		prev_tmp->next = new_node;
+		new_node->prev = prev_tmp;
+		new_node->next = tmp;
+		if (tmp != NULL)
+			tmp->prev = new_node;
+		return (new_node);
+	}
+	return (NULL);
 }
 
