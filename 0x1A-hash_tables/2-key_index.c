7ea44cffa65e096a31b57c4b8784669526b06c44
EduardoVega04 <eduarvega04@hotmail.com>
2020-05-30 01:10:05 +0000

    Get the assigned index in the hash table for an entry

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+/**
+ * key_index - Returns the assigned index for a key
+ * @key: string used to generate hash value
+ * @size: The size of the array (hash table)
+ * Return: Index in hash table
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	if (!key)
+		return (-1);
+
+	return (hash_djb2(key) % size);
+}
