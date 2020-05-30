0db983a35760e7314957e96cacc09fa3408934a6
EduardoVega04 <eduarvega04@hotmail.com>
2020-05-30 03:22:48 +0000

    Add an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -61,7 +61,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 	}
 
-	ht->array[index] = add_node_beginning(&(ht->array[index]), dup_key, dup_value);
+	ht->array[index] = add_node_beginning(&ht->array[index], dup_key, dup_value);
 	if (ht->array[index] == NULL)
 		return (0);
 
