9e7bd82ec707794e0fba0de9353cefb9f8cb44f1
EduardoVega04 <eduarvega04@hotmail.com>
2021-01-10 18:20:00 -0500

    Set an entry in hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,42 +1,25 @@
 #include "hash_tables.h"
 /**
- * add_node - Adds a node at the beginning of a linked list
- * @head: Head of the linked list (pointer in the hash table index)
+ * new_entry - Creates a new entry and returns it
  * @key: Key to add
  * @value: Value of the key
  * Return: The address of the new node
  */
-hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
+hash_node_t *new_entry(const char *key, const char *value)
 {
 	hash_node_t *new_node = NULL;
-	char *dup_key = strdup(key);
-	char *dup_value = strdup(value);
-
-	if (!dup_key)
-		return (NULL);
-
-	if (!dup_value)
-	{
-		free(dup_key);
-		return (NULL);
-	}
 
 	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
-	{
-		free(dup_value);
-		free(dup_key);
-		return (NULL);
-	}
+	new_node->key = malloc(strlen(key) + 1);
+	new_node->value = malloc(strlen(value) + 1);
 
-	new_node->key = dup_key;
-	new_node->value = dup_value;
-
-	new_node->next = *head;
-	*head = new_node;
+	strcpy(new_node->key, key);
+	strcpy(new_node->value, value);
+	new_node->next = NULL;
 
 	return (new_node);
 }
+
 /**
  * hash_table_set - Adds an element in the hash table
  * @ht: Pointer to the hash table
@@ -46,32 +29,38 @@ hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	char *dup_value = NULL;
-	hash_node_t *tmp = NULL;
+	unsigned long int index = 0;
+	hash_node_t *entry = NULL;
+	hash_node_t *add_new = NULL;
 
-	if (!ht || !key || strlen(key) == 0 || !value)
+	if (!ht || !key || !value)
 		return (0);
 
-	index = key_index((unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
+	entry = ht->array[index];
 
-	for (tmp = ht->array[index]; tmp != NULL; tmp = tmp->next)
+	if (entry == NULL)
 	{
-		if (strcmp(tmp->key, key) == 0)
-		{
-			dup_value = strdup(value);
-			if (dup_value == NULL)
-				return (0);
-
-			free(tmp->value);
-			tmp->value = dup_value;
-			return (1);
-		}
+		entry = new_entry(key, value);
+		return (1);
 	}
 
-	if (add_node(&(ht->array[index]), key, value) == NULL)
-		return (0);
+	while (entry != NULL)
+	{
+		if (strcmp(entry->key, key) == 0)
+		{
+			free(entry->value);
+			entry->value = malloc(strlen(value) + 1);
+			strcpy(entry->value, value);
+			return (1);
+		}
+
+		entry = entry->next;
+	}
+
+	add_new = new_entry(key, value);
+	add_new->next = ht->array[index];
+	ht->array[index] = add_new;
 
 	return (1);
 }
-
