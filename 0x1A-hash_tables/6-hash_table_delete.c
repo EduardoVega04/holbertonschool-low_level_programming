56c2f7d8eb0b5f2b2905a7d45775852104f73cb9
EduardoVega04 <eduarvega04@hotmail.com>
2021-01-10 20:20:39 -0500

    Delete a hash table

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -11,7 +11,7 @@ void hash_table_delete(hash_table_t *ht)
 
 	if (!ht)
 		return;
-	
+
 	for (; i < ht->size; i++)
 	{
 		eraser = ht->array[i];
@@ -23,7 +23,7 @@ void hash_table_delete(hash_table_t *ht)
 			free(prev->key);
 			free(prev->value);
 			free(prev);
-		}		
+		}
 	}
 
 	free(ht->array);
